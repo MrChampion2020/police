7ad749ee5a8b51435adcd63f5254f18c3eb4a6ae
MrChampion2020 <kingedendgreat2017@gmail.com>
2025-01-08 13:21:59 +0100

    merge remote change

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-// const baseUrl = '';
-// export default baseUrl;
-
-const baseUrl = 'http://localhost:5000';
+const baseUrl = 'https://api-police.onrender.com';
 export default baseUrl;
+
+// const baseUrl = 'http://localhost:5000';
+// export default baseUrl;
 
 // const k = "hi";
